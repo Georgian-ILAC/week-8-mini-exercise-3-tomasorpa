b7e9e9f7db1832386d540d6903b122ea15417b49
tomasorpa <tomasorpa@gmail.com>
2024-07-06 13:17:41 -0400

    Assignment finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee tomas=new Employee("Tomas","Ortega",1.75,123456789,
+                "Colombian",false,"Junior Developer",1,123312421,20);
+
+        System.out.println(tomas.getHourlyPay());
+        tomas.payDay(45);
+        System.out.println(tomas.toString());
     }
 }
